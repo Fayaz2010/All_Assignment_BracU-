42ba4fdeadb62485a735069c6a6d961c9b8f434d
Fayaz2010 <fayazfardin2023@gmail.com>
2025-01-25 16:31:39 +0600

    Add files via upload

---
+++ CSE111/Assignment_9/Task_1/BBAStudent.java
@@ -0,0 +1,15 @@
+
+
+public class BBAStudent extends Student{
+   public BBAStudent(){
+     updateDepartment("BBA" );
+   
+updateName("Default" );
+   } 
+
+public BBAStudent(String str){
+ updateDepartment("BBA" );
+  updateName(str );
+}
+}
+
