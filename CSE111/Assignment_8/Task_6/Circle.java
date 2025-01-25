be3830a5ee332c0e803c71c86a250b81a6a293eb
Fayaz2010 <fayazfardin2023@gmail.com>
2025-01-25 16:28:02 +0600

    Add files via upload

---
+++ CSE111/Assignment_8/Task_6/Circle.java
@@ -0,0 +1,9 @@
+public class Circle extends Shape {
+ public int radius;
+ public double area;
+ public void area(){
+    area=(3.1416*radius*radius);
+ System.out.println("Area of "+color+" "+name+": "+area);
+
+ }
+}
